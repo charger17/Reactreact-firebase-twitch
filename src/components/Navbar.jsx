1dc1b7ecce69e79a9c55672d7b97e4561fe7628d
Carlos Perez Salazar <cps172779@gmail.com>
2023-05-29 14:20:45 -0600

    Simple auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,29 @@ import { NavLink } from "react-router-dom";
 import { Usercontext } from "../context/UserProvider";
 
 const Navbar = () => {
-  const { user, setUser } = useContext(Usercontext);
+  const { user, signOutUser } = useContext(Usercontext);
+
+  const handleClickLogOut = async () => {
+    try {
+      await signOutUser();
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+
   return (
     <>
       <div>
         {user ? (
-            <>
-                <NavLink to="/">Inicio</NavLink>
-                <button onClick={() => setUser(false)}>Logout</button>
-            </>
+          <>
+            <NavLink to="/">Inicio</NavLink>
+            <button onClick={handleClickLogOut}>Logout</button>
+          </>
         ) : (
-          <NavLink to="/login">Login</NavLink>
+          <>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </>
         )}
       </div>
     </>
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCK6Nbsl2alSz8kqYAKB9EEuZnCJZ6uOOA",
+  authDomain: "react-2022-36ab5.firebaseapp.com",
+  projectId: "react-2022-36ab5",
+  storageBucket: "react-2022-36ab5.appspot.com",
+  messagingSenderId: "396274352415",
+  appId: "1:396274352415:web:1c0c59c030098340a4f02a",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
